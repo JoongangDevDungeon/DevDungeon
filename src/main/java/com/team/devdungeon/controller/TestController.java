8fe350f3e48a3d1ac03b14be6cfae2d0a2f687d4
User <User@HONG>
2023-02-23 18:43:27 +0900

    ㅇㅈㅈㄹㅈㄹㄷㅈㄹㅈㅈㄷㄹㄹㅈㄷㅈㄷㄹㅈㄷㄷ

--- src/main/java/com/team/devdungeon/controller/TestController.java
+++ src/main/java/com/team/devdungeon/controller/TestController.java
@@ -13,7 +13,7 @@ public class TestController {
 
     @GetMapping("/index")
     public String index() {
-    	System.out.println("홍준호님꺼입니다.");
+    	System.out.println("홍준호님");
     	int count = testService.boardCount();
         System.out.println("결과 : " + count);
         return "index";
