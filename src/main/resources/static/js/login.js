6a612e4abc9a1092af56f737356d1d1079943f97
HONG\User <wnsg66@naver.com>
2023-03-20 15:18:46 +0900

    top login 수정중

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,24 +1,33 @@
 $(function() {
-
 	let level = localStorage.getItem("member_level");
 	let point = localStorage.getItem("member_point");
  	if (level) { 
   		$("#member_level").text("Lv."+level+"  ");
  		$("#member_point").text("포인트 : "+point);
   	}
+    $("#member_point").click(function(){
+        // $.ajax({
+        //     type : "post",
+        //     url : "/login",
+        //     dataType : "json",
+        //     data : {"member_id" : member_id, "member_pw" : member_pw},
+        //     success : function (result) {
+        //         localStorage.setItem("member_level", result.member_level);
+        //         localStorage.setItem("member_point", result.member_point);
+        //         // level = result.member_level;
+        //         // point = result.member_point;
+        //         $("#member_level").text("Lv."+level+"  ");
+        //         $("#member_point").text("포인트 : "+point);
+        //     },
+        //     error : function(xhr){
+        //         alert("오류 발생");
+        //     }
+        // });
 
-         $.ajax({
-             type : "post",
-             url : "/login",
-             data : {"member_id" : member_id, "member_pw" : member_pw},
-             dataType : "json",
-             success : function (result) {
-                 level = result.member_level;
-                 point = result.member_point;
-                 $("#member_level").text("Lv."+level+"  ");
-                 $("#member_point").text("포인트 : "+point);
-             }
-         });
+
+    });
+
+
 
     $("#member_id").on("keyup",function(key){
         if(key.keyCode==13) {
