0ee3815d3f1c25a9af98c4886cf4d44128dcbf50
Yuj-Jung-Mok <60291344+Yuj-Jung-Mok@users.noreply.github.com>
2023-03-27 17:16:36 +0900

    로그인 - 로그인 실패 시 동작 변경

--- src/main/java/com/team/devdungeon/controller/SignController.java
+++ src/main/java/com/team/devdungeon/controller/SignController.java
@@ -35,7 +35,6 @@ public class SignController {
     @PostMapping("/index")
     @ResponseBody
     public String index(HttpServletRequest request, @RequestParam String error) {
-        System.out.println(error);
         return "아이디 혹은 비밀번호를 다시 확인해주세요.";
     }
 
